a88cc92dbcb7fdd557e24f638883f003c78b758c
PremierMinistre <nomail>
2020-11-15 12:04:30 +0000

    Original commit

---
+++ src/hue_client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_client_hue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World HUE!");
+        }
+    }
+}
